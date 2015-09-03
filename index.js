139e75e1ad19bb77dcc94954eea809c754c89da6
bergi <bergi@axolotlfarm.org>
2015-09-03 19:36:45 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,80 @@
+var util = require('util')
+var AbstractSerializer = require('rdf-serializer-abstract')
+
+function JsonLdSerializer (rdf) {
+  AbstractSerializer.call(this, rdf)
+}
+
+util.inherits(JsonLdSerializer, AbstractSerializer)
+
+JsonLdSerializer.prototype.serialize = function (graph, done) {
+  return new Promise(function (resolve) {
+    done = done || function () {}
+
+    var jsonGraph = []
+    var subjects = {}
+
+    var subjectIndex = function (subject) {
+      var value = subject.valueOf()
+
+      if (typeof subjects[value] === 'undefined') {
+        if (subject.interfaceName === 'BlankNode') {
+          jsonGraph.push({ '@id': '_:' + value })
+        } else {
+          jsonGraph.push({ '@id': value })
+        }
+
+        subjects[value] = jsonGraph.length - 1
+      }
+
+      return subjects[value]
+    }
+
+    var objectValue = function (object) {
+      var value = object.valueOf()
+
+      if (object.interfaceName === 'NamedNode') {
+        return { '@id': value }
+      } else if (object.interfaceName === 'BlankNode') {
+        return { '@id': '_:' + value }
+      } else {
+        if (object.language) {
+          return { '@language': object.language, '@value': value }
+        } else if (object.datatype && object.datatype.valueOf() !== 'http://www.w3.org/2001/XMLSchema#string') {
+          return { '@type': object.datatype.valueOf(), '@value': value }
+        } else {
+          return value
+        }
+      }
+    }
+
+    graph.forEach(function (triple) {
+      var index = subjectIndex(triple.subject)
+      var predicateValue = triple.predicate.valueOf()
+
+      if (predicateValue === 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type') {
+        if (typeof jsonGraph[index]['@type'] === 'undefined') {
+          jsonGraph[index]['@type'] = []
+        }
+
+        jsonGraph[index]['@type'].push(triple.object.valueOf())
+      } else {
+        if (typeof jsonGraph[index][predicateValue] === 'undefined') {
+          jsonGraph[index][predicateValue] = objectValue(triple.object)
+        } else {
+          if (!Array.isArray(jsonGraph[index][predicateValue])) {
+            jsonGraph[index][predicateValue] = [jsonGraph[index][predicateValue]]
+          }
+
+          jsonGraph[index][predicateValue].push(objectValue(triple.object))
+        }
+      }
+    })
+
+    done(null, jsonGraph)
+
+    resolve(jsonGraph)
+  })
+}
+
+module.exports = JsonLdSerializer
---
+++ test/test.js
@@ -0,0 +1,91 @@
+/* global describe, it */
+
+var assert = require('assert')
+var jsonld = require('jsonld')
+var rdf = require('rdf-ext')()
+var testData = require('rdf-test-data')(rdf)
+var testUtils = require('rdf-test-utils')(rdf)
+var JsonLdSerializer = require('../').bind(null, rdf)
+
+describe('JSON-LD serializer', function () {
+  var simpleGraph = rdf.createGraph()
+
+  simpleGraph.add(rdf.createTriple(
+    rdf.createNamedNode('http://example.org/subject'),
+    rdf.createNamedNode('http://example.org/predicate'),
+    rdf.createLiteral('object')
+  ))
+
+  describe('callback API', function () {
+    it('should be supported', function (done) {
+      var serializer = new JsonLdSerializer()
+
+      Promise.resolve(new Promise(function (resolve, reject) {
+        serializer.serialize(simpleGraph, function (error) {
+          if (error) {
+            reject(error)
+          } else {
+            resolve()
+          }
+        })
+      })).then(function () {
+        done()
+      }).catch(function (error) {
+        done(error)
+      })
+    })
+  })
+
+  describe('Promise API', function () {
+    it('should be supported', function (done) {
+      var serializer = new JsonLdSerializer()
+
+      serializer.serialize(simpleGraph).then(function () {
+        done()
+      }).catch(function (error) {
+        done(error)
+      })
+    })
+  })
+
+  describe('Stream API', function () {
+    it('should be supported', function (done) {
+      var serializer = new JsonLdSerializer()
+      var jsonGraph
+
+      serializer.stream(simpleGraph).on('data', function (data) {
+        jsonGraph = data
+      }).on('end', function () {
+        if (!jsonGraph) {
+          done('no data streamed')
+        } else {
+          done()
+        }
+      }).on('error', function (error) {
+        done(error)
+      })
+    })
+  })
+
+  describe('example data', function () {
+    it('card should be serialized', function (done) {
+      var serializer = new JsonLdSerializer()
+
+      Promise.all([
+        serializer.serialize(testData.cardGraph).then(function (cardJson) {
+          return jsonld.promises().normalize(cardJson)
+        }),
+
+        testUtils.p.readFile('support/card.json', __dirname).then(function (cardString) {
+          return jsonld.promises().normalize(JSON.parse(cardString))
+        })
+      ]).then(function (results) {
+        assert.deepEqual(results[0], results[1])
+      }).then(function () {
+        done()
+      }).catch(function (error) {
+        done(error)
+      })
+    })
+  })
+})
